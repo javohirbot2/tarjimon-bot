de5e133d893aa7169f7fbceb06a64a5bd06708a9
Developer Rasulovich <97433981+javohirbot2@users.noreply.github.com>
2022-01-27 13:46:44 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,8 @@ from googletrans import Translator
 
 updater = Updater('5163195332:AAFe3nV2dcaVIGjH7MyXRjldpeg5OEf7bnc',use_context = True )
 
-def start(update,context):
-  user = update.message.from_user
- update.message.reply_html('👋🏻Assalomu alaykum <b>{}!</b>\n \n<b> Ushbu bot orqali\n matnlarni eng-uzb shaklida\n tarjima qila olasiz.\n Botga matn yuboring</b>'.
-                           format(user.first_name))
+def start(updater,context):
+ updater.message.reply_html('👋🏻Assalomu alaykum <b>{}!</b>\n \n<b> Ushbu bot orqali\n matnlarni eng-uzb shaklida\n tarjima qila olasiz.\n Botga matn yuboring</b>.')
  
 def echo(updater,context):
  usr_msg =updater.message.text
