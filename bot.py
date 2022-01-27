bacee749962aa8362db35a0652040ed40580324f
Developer Rasulovich <97433981+javohirbot2@users.noreply.github.com>
2022-01-27 13:28:37 +0500

    bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+from telegram.ext import Updater,MessageHandler,CommandHandler,Filters
+from googletrans import Translator
+
+updater = Updater('BOT TOKEN',use_context = True )
+
+def start(updater,context):
+  user = updater.message.from_user
+ updater.message.reply_html('👋🏻Assalomu alaykum <b>{}!</b>\n \n<b> Ushbu bot orqali\n matnlarni eng-uzb shaklida\n tarjima qila olasiz.\n Botga matn yuboring</b>'.
+                           format(user.first_name))
+ 
+def echo(updater,context):
+ usr_msg =updater.message.text
+ translator = Translator()
+ translation = translator.translate(usr_msg,dest='uz') 
+ updater.message.reply_text(translation.text)
+ 
+dp =updater.dispatcher.add_handler
+dp(CommandHandler('start',start))
+dp(MessageHandler(Filters.text,echo))
+
+updater.start_polling()
+updater.idle()
